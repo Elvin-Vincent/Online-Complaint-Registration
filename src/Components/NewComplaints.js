d5fe3513c4854410aca5873c3767bcee550f6ec1
Elvin <elvinva2271@gmail.com>
2023-08-20 17:21:38 +0530

    modified the code

---
+++ src/Components/NewComplaints.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewComplaints = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default NewComplaints
--- src/Components/OfficerContent.js
+++ src/Components/OfficerContent.js
@@ -1,27 +1,39 @@
 import React from "react";
 import './OfficerContent.css';
+import { Route, Routes } from "react-router-dom";
+import NewComplaints from "./NewComplaints";
 
 function OfficerContent() {
   return (
-    <div>
-      <div className="head">
-      <h1> Officer Dashboard</h1>
+    <div className="officer-content">
+      <div className="sidebar">
+        <div className="head">
+          <h1>Officer Dashboard</h1>
+        </div>
+        <ul className="navigation-list">
+        <li>
+          <a href="#view-new-complaint">View New Complaint</a>
+        </li>
+        <li>
+          <a href="#view-profile">View Profile</a>
+        </li>
+        <li>
+          <a href="#view-complaint-history">View Complaint History</a>
+        </li>
+        <li>
+          <a href="#view-feedback">View Feedback</a>
+        </li>
+      
+        <li className="logout">
+          <a href="#logout">Logout</a>
+        </li>
+        </ul>
       </div>
-      <ul className="list" >
-        <li>
-          view profile
-        </li>
-        <li>
-          view new Complaint
-        </li>
-        <li>
-          view complaint history
-        </li>
-        <li>
-          view feedback
-        </li>
-      </ul>
-     
+      <div className="content">
+        <Routes>
+        <Route index element={< NewComplaints/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
